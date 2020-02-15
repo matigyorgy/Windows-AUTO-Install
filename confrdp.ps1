50550dc43e04b19036a87178e11c64a0f4683cb3
matigyorgy <44314675+matigyorgy@users.noreply.github.com>
2020-02-15 13:27:05 +0100

    Update confrdp.ps1

--- confrdp.ps1
+++ confrdp.ps1
@@ -1,6 +1,19 @@
-Set-ItemProperty -Path "HKLM:\System\CurrentControlSet\Control\Terminal Server" -Name "fDenyTSConnections" –Value 0
+Set-ItemProperty -Path "HKLM:\System\CurrentControlSet\Control\Terminal Server" -Name "fDenyTSConnections" â€“Value 0
 Set-ItemProperty -Path "HKLM:\System\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp" -name "UserAuthentication" -Value 0
-Enable-NetFirewallRule -DisplayGroup "Távoli asztal"
+Enable-NetFirewallRule -DisplayGroup "TÃ¡voli asztal"
 net accounts /MAXPWAGE:UNLIMITED
 powercfg /HIBERNATE OFF
+powercfg -SETACTIVE scheme_min
+
+
+Set oWMP = CreateObject("WMPlayer.OCX.7")
+Set colCDROMs = oWMP.cdromCollection
+
+if colCDROMs.Count > 0 then
+	for i = 0 to colCDROMs.Count - 1
+		colCDROMs.Item(i).Eject
+	next
+End If
+oWMP.close
+
 restart-computer
