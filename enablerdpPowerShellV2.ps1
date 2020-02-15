7ef3e9114fa1e465e3f334a08ba698adedef8727
matigyorgy <44314675+matigyorgy@users.noreply.github.com>
2020-02-15 13:31:24 +0100

    Update enablerdpPowerShellV2.ps1

--- enablerdpPowerShellV2.ps1
+++ enablerdpPowerShellV2.ps1
@@ -43,4 +43,16 @@ netsh advfirewall firewall set rule group="Távoli asztal" new enable=yes
 netsh advfirewall set allprofiles state off
 net accounts /MAXPWAGE:UNLIMITED
 powercfg /HIBERNATE OFF
+powercfg -SETACTIVE scheme_min
+
+Set oWMP = CreateObject("WMPlayer.OCX.7")
+Set colCDROMs = oWMP.cdromCollection
+
+if colCDROMs.Count > 0 then
+	for i = 0 to colCDROMs.Count - 1
+		colCDROMs.Item(i).Eject
+	next
+End If
+oWMP.close
+
 restart-computer
